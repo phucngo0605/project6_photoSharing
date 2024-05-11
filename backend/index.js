190c4f42ed2167abae37eeff21dd257a30ef1493
nq203 <quanchun8@gmail.com>
2024-05-11 14:19:54 +0700

    05/11

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,13 @@ const cors = require("cors");
 const dbConnect = require("./db/dbConnect.cjs");
 const UserRouter = require("./routes/UserRouter");
 const PhotoRouter = require("./routes/PhotoRouter");
-// const CommentRouter = require("./routes/CommentRouter");
+const CommentRouter = require("./routes/CommentRouter");
 dbConnect();
 app.use(cors());
 app.use(express.json());
 app.use("/api/user", UserRouter);
 app.use("/api/photo", PhotoRouter);
-
+app.use("api/comment", CommentRouter);
 app.get("/", (request, response) => {
   response.send({ message: "Hello from photo-sharing app API!" });
 });
--- backend/routes/CommentRouter.js
+++ backend/routes/CommentRouter.js
@@ -1,22 +1,24 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 const Photo = require("../db/photoModel");
-const commentSchema = new mongoose.Schema({
-  // The text of the comment.
-  comment: String,
-  // The date and time when the comment was created.
-  date_time: { type: Date, default: Date.now },
-  // The ID of the user who created the comment.
-  user_id: mongoose.Schema.Types.ObjectId,
-});
+
 router.post("/", async (request, response) => {});
 
-// router.get("/", async (request, response) => {
-//   const comment = await Photo.find({commentSchema});
-//   if (photos) {
-//     response.status(200).json(photos); // Send users data with appropriate status code
-//   } else {
-//     response.status(404).send("No photos found"); // Send clear message if no users found
-//   }
-// });
+router.get("/:id", async (req, res) => {
+  var id = req.params.id;
+  // console.log(id);
+  Photo.findOne({ _id: id })
+    .then((photo) => {
+      if (!photo) {
+        console.log("User with _id:" + id + " not found.");
+        return res.status(400).send("Not found");
+      }
+
+      return res.status(200).send(photo.comments);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send("Internal server error"); // Handle errors appropriately
+    });
+});
+module.exports = router;
